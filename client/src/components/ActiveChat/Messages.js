5b3f66c8c674560fd159b044bb8fa8b36c5fa8c2
sumin <kim.sumin.wat2014@gmail.com>
2021-10-06 20:36:22 -0700

    created findLatestMessageId function to find read message

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -6,13 +6,32 @@ import moment from "moment";
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
 
+  // to find messsage which already read
+  const findLatestMessageId = () => {
+    let latestMessageId = null;
+    messages.forEach((message) => {
+      if (message.senderId === userId && message.isRead) {
+        latestMessageId = message.id;
+      }
+    });
+
+    return latestMessageId;
+  };
   return (
     <Box>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble
+            key={message.id}
+            messageId={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+            isRead={message.isRead}
+            latestMessageId={findLatestMessageId()}
+          />
         ) : (
           <OtherUserBubble
             key={message.id}
