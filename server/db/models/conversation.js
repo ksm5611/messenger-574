e0c9784e9b7bcc2f32bd1481ce5e01d56a08e4a2
sumin <kim.sumin.wat2014@gmail.com>
2021-10-09 21:37:15 -0700

    created join table between user and conversation

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -4,22 +4,6 @@ const Message = require("./message");
 
 const Conversation = db.define("conversation", {});
 
-// find conversation given two user Ids
-
-Conversation.findConversation = async function (user1Id, user2Id) {
-  const conversation = await Conversation.findOne({
-    where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id]
-      },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
-      }
-    }
-  });
-
-  // return conversation or null if it doesn't exist
-  return conversation;
-};
+// when the userConversation.js is created then we don't need to find user1Id, user2Id anymore.
 
 module.exports = Conversation;
